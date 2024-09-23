cda3fb3b9ab0d6152af18b2cf78ac637b5c9d3c2
endawghae <94265058+endawghae@users.noreply.github.com>
2024-09-22 18:03:03 -0700

    multi page setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import countries from './pages/countries'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/countries" element={<Countries />} />
+            </Routes>
+        </Router>
+    );
+}
+ 
+export default App;
+
+//ReactDOM.createRoot(document.getElementById('root')).render(<App />)
